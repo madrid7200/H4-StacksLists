e46558afb3737d2b65ac6be4405e7d64fc0b43c9
madrid7200 <106935787+madrid7200@users.noreply.github.com>
2023-02-19 18:37:36 -0600

    Init

---
+++ src/ICalculator.java
@@ -0,0 +1,12 @@
+
+public interface ICalculator {
+	
+	public float Add(float number1, float number2);
+	
+	public float Substraction(float number1, float number2);
+	
+	public float Multiply(float number1, float number2);
+	
+	public float Divition(float number1, float number2);
+	
+}
